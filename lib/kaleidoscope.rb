e183d20e503362d1487407850cb3356ade269809
Nicolas Sanguinetti <contacto@nicolassanguinetti.info>
2009-01-22 05:12:50 -0200

    Add some documentation

--- lib/kaleidoscope.rb
+++ lib/kaleidoscope.rb
@@ -1,10 +1,26 @@
 module Kaleidoscope
-  def self.factory(model, name=nil, definition=nil, &block)
+  # Setup simple and clean factories for activerecord using named_scopes
+  # (no touching of the internals!)
+  # 
+  #   Kaleidoscope.factory(User) {{ 
+  #     :email => Faker::Internet.email, 
+  #     :password => "test", 
+  #     :password_confirmation => "test" 
+  #   }}
+  #
+  #   User.kaleidoscope.create #=> #<User id: 1, email: "foo@bar.com", ...>
+  #   User.kaleidoscope.new    #=> #<User id: nil, email: "bar@foo.com" ...>
+  # 
+  #   Kaleidoscope.factory(User, :admin) {{
+  #     :admin => true
+  #   }}
+  #
+  #   User.admin.create #=> #<User id: 1, admin: true, ...>
+  #
+  def self.factory(model, name=nil, &block)
     name ||= :kaleidoscope
-    definition = definition || block.call
+    definition = block.call
     definition = { :conditions => definition } if Hash === definition
-    model.class_eval do
-      named_scope name, definition
-    end
+    model.named_scope name, definition
   end
 end
